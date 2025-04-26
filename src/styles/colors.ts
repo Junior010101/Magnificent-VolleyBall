19d6461c22a879345e7758f29c9c6636e9ee52fd
Junior010101 <marcondesjunior2209@gmail.com>
2025-04-26 01:49:41 -0300

    Mandando o Projeto estruturado para o github.

---
+++ src/styles/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+    white: '#FFFF',
+
+    gray: {
+        200: '#C4C4C4',
+        300: '#8D8D8D'
+    }
+}
---
+++ src/styles/fonts.ts
@@ -0,0 +1,3 @@
+export const fontFamily = {
+    regular: "Roboto_400Regular",
+}
